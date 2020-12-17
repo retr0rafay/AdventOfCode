6e4154c720b17368008248466c1fe0f9c2de8889
Rafay Syed <rafaysyeda@live.com>
2020-12-16 23:59:16 -0500

    Solved part 2 of the Day One challenge

--- DayOne.py
+++ DayOne.py
@@ -1,6 +1,8 @@
 import csv
 import math
 
+# Part 1
+
 list_of_nums = []
 with open('input.txt') as file:
     csv_reader = csv.reader(file)
@@ -16,3 +18,18 @@ for num in list_of_nums:
         break
 
 print(result_nums[0] * result_nums[1])
+
+# Part 2
+
+result_nums = []
+for i in range(len(list_of_nums)):
+    split_number = 2020 - list_of_nums[i]
+    for j in range(len(list_of_nums)):
+        if split_number - list_of_nums[j] in list_of_nums:
+            result_nums.append(list_of_nums[i])
+            result_nums.append(list_of_nums[j])
+            result_nums.append(split_number - list_of_nums[j])
+            break
+    if len(result_nums) == 3:
+        break
+print(result_nums)
